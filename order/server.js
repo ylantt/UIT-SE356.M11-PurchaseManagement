416e97ac4900e9e563e43304f9d7abece381fa6c
Minhthang1506 <19522216@gm.uit.edu.vn>
2022-01-10 16:19:03 +0700

    add Loading Balance for Order service

--- order/server.js
+++ order/server.js
@@ -1,6 +1,9 @@
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const cluster = require('cluster')
 const app = require('./app')
+
+const numCPUs = require('os').cpus().length;
 
 dotenv.config({ path: '../config.env' })
 const port = process.env.ORDER_PORT || 5006
@@ -18,6 +21,30 @@ mongoose
   .then(() => console.log('DB connection successful'))
   .catch((err) => console.log(err))
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`)
-}) // start the server
+// For Master process
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+ 
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+ 
+  // This event is firs when worker died
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+}
+ 
+// For Worker
+else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  app.listen(port, err => {
+    err ?
+      console.log("Error in server setup") :
+      console.log(`Worker ${process.pid} started on port ${port}`);
+  });
+ 
+}
+
